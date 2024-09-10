a5d5f8998024e47c117a2ccc4bc39405d46e2a7e
rwalk651 <user@example.com>
2024-09-10 05:13:37 -0400

    Added docstrings

--- camelcase.py
+++ camelcase.py
@@ -10,10 +10,11 @@ def camelcase(sentence):
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
 def instructions():
+    """ Display instructions for how to use the program """
     print('Enter a sentence and this program will convert it to camelcase.\n')
 
 def banner():
-    """ Display program name """
+    """ Display program name using stars """
     message = 'Awesome camelcase program!'
     stars = '*' * len(message)
     print(f'\n{stars} \n{message} \n{stars}\n')
