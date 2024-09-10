f253095d8ee79b3fb4e8677fea60d62c505c8b6c
rwalk651 <user@example.com>
2024-09-10 04:54:33 -0400

    Added banner welcome message at start of program

--- camelcase.py
+++ camelcase.py
@@ -1,4 +1,3 @@
-
 def camelcase(sentence):
     """ Convert sentence to camelCase, for example, "Display all books"
     is converted to "displayAllBooks" """
@@ -10,7 +9,14 @@ def camelcase(sentence):
     # So this still works on empty strings, strings of length 1
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    """ Display program name """
+    message = 'Awesome camelcase program!'
+    stars = '*' * len(message)
+    print(f'\n{stars} \n{message} \n{stars}\n')
+
 def main():
+    banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
